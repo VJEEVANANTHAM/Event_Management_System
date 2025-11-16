e0010835817672c02b6bfca79579d1116e6faea5
VJEEVANANTHAM <105180442+VJEEVANANTHAM@users.noreply.github.com>
2025-11-16 11:36:21 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://event-management-system-backend-1vwr.onrender.com/",
+  baseURL: "https://event-management-system-backend-1vwr.onrender.com/api",
 });
 
 export default API;
